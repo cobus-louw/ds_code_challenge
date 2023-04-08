304096a9080d98ca6ecf3d95fa6249ea0d7caa42
cobus-louw <jmlouw96+1@gmail.com>
2023-04-08 22:56:27 +0200

    fix: :ambulance: Import added to __init__ of package

--- src/cptcc/__init__.py
+++ src/cptcc/__init__.py
@@ -3,6 +3,7 @@ from cptcc.cptcc import *
 import cptcc.distance as distance
 import cptcc.utils as utils
 import cptcc.wind as wind
+import cptcc.anonymize as anonymize
 
 from importlib.metadata import version
 __version__ = version("cptcc")
